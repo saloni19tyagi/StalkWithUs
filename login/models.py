1a4b8066ac17c588d7ed81c3dfc7b22e2b3f0554
Saloni Tyagi <saloni19tyagi@gmail.com>
2020-04-11 17:37:25 +0000

    Revert "Merge branch 'vandita' into 'master'"
    This reverts merge request !4

--- login/models.py
+++ login/models.py
@@ -1,7 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class userdata(models.Model):
-	username = models.CharField(max_length=20)
-	password = models.CharField(max_length=20)
-	email =models.CharField(max_length=20)
