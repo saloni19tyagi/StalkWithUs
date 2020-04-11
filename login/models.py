f4bdebbaf7d5300c1c96117f2592a8a14198bd61
Saloni Tyagi <saloni19tyagi@gmail.com>
2020-04-11 23:35:04 +0530

    Test

--- login/models.py
+++ login/models.py
@@ -6,5 +6,4 @@ class userdata(models.Model):
     username = models.CharField(max_length=20)
 	password = models.CharField(max_length=20)
 	email =models.CharField(max_length=20)
-=======
->>>>>>> login/models.py
+
