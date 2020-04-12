612edb30616032979b9f22dd9f2b0e5403315a62
Vandita Singh <you@example.com>
2020-04-12 23:35:08 +0530

    nothing changed

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render
 # import psutil
 # import socket
 from django.http import HttpResponse
-
-import datetime
-from django.conf import settings
 from django.shortcuts import redirect
 from django.contrib.auth import logout
 # Create your views here.
