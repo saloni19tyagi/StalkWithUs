75bda3ef72e2cc96e7f3d49fb0206181205f3915
Saloni Tyagi <saloni19tyagi@gmail.com>
2020-04-21 18:27:14 +0530

    Data fetch API from Codeforces

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,6 +5,15 @@ from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.contrib.auth import logout
 # Create your views here.
+from codeforces import api
+import requests
 
 def home(request):
-    return render(request, "index.html", {})
+    return render(request, 'index.html')
+
+def viewProfile(request):
+    url = "https://codeforces.com/api/user.info?handles=vandita_1081"
+    response = requests.request("GET", url)
+    print(response.text.encode('utf8'))
+    response = response.json()
+    return render(request, 'profile.html', {'submissionData': response})
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/salonityagi/Documents/Projects/stalkwithus/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
