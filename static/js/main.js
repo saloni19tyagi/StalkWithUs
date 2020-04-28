2444b8b492dbeddf78486a56c3447d0fe477200f
Vandita Singh <you@example.com>
2020-04-28 21:31:56 +0530

    bootstrap & fontawesome locally added

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,6 @@
  var closeNav = false;
 function openNav() {
   closeNav = !closeNav;
-  console.log(closeNav);
   if(closeNav){
   document.getElementById("mySidenav").style.width = "200px";
   document.getElementById("main").style.marginLeft = "200px";
