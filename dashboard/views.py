91575e43f9657116957fe1f9da294d20e0a07e06
Saloni Tyagi <saloni19tyagi@gmail.com>
2020-04-29 10:59:44 +0530

    Modified Views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -14,18 +14,12 @@ def home(request):
 		codechef_url = 'https://competitive-coding-api.herokuapp.com/api/codechef/%s' % request.session['codechef']
 		response = requests.get(codechef_url)
 		codechef = response.json()
-		url = "https://codeforces.com/api/user.info?handles="+request.session['handle']
 
-		payload = {}
-		headers = {
-			'Cookie': 'JSESSIONID=02249090B1061D683A8B443894284151-n1; 39ce7=CFkwUcjt'
-		}
-		response = requests.request("GET", url, headers=headers, data = payload)
+		codeforcesurl = "https://codeforces.com/api/user.info?handles="+request.session['codeforces']
+		codeforces = requests.request("GET", codeforcesurl).json()
+		li = codeforces['result']
+		codeforcesrating = li[0]
 
-		response = response.json()
-		li = response['result']
-		data = li[0]
-
-		return render(request, "index.html", {'codechefRank': codechef['rank'], 'codeforcesRating': data['rating']})
+		return render(request, "index.html", {'codechefRank': codechef['rank'], 'codeforcesRating': codeforcesrating['rating']})
 	return redirect('/login/?next=%s' % request.path)
 
